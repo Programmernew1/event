ab29a4b2c1b4bb3b1f00325908ae460eef5e8df1
Pavel <p.zinovkin@mail.ru>
2020-12-21 23:52:02 +0300

    1

---
+++ index.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded',function(){
+document.querySelector('#Klik').addEventListener('click',function(){
+document.querySelector('#pp').classList.toggle('ps')
+})
+})
